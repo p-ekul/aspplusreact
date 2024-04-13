c3a8c344b4f5ee0e17a7413f9543887d5c9a8218
pekul <luke1682@gmail.com>
2024-04-13 21:52:36 +0900

    upto

--- aspworks.client/src/App.jsx
+++ aspworks.client/src/App.jsx
@@ -1,41 +1,56 @@
 import './App.css';
-import ApiCalendar from "react-google-calendar-api";
-import { useSession, useSupabaseClient } from
-    "@supabase/auth-helpers-react";
+import { useState } from 'react';
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+//import DateTimePicker from "react-datetime-picker";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+
 function App() {
     const session = useSession();
     const supabaseClient = useSupabaseClient();
+    const [start, setStart] = useState(new Date());
+    const [endd, setEnd] = useState(new Date());    
 
-    //async function signIn() {
-    //    const { error} = await supabaseClient.auth.signInWithOAuth({
-    //        provider: 'google',
-    //        options: {
-    //            scopes: 'https://www.googleapis.com/auth/calendar'
-    //        }
-    //    }); 
-    //    if (error) {
-    //        console.error(error.message);
-    //        alert(error.message);
-    //    }
-    //}
-    //async function signOut() {
-    //    await supabaseClient.auth.signOut();
-    //}
+    async function signIn() {
+        const { error} = await supabaseClient.auth.signInWithOAuth({
+            provider: 'google',
+            options: {
+                scopes: 'https://www.googleapis.com/auth/calendar'
+            }
+        }); 
+        if (error) {
+            console.error(error.message);
+            alert(error.message);
+        }
+    }
+    async function signOut() {
+        await supabaseClient.auth.signOut();
+    }
 
     
     return (
-        <div className="App">
-            <h1>HELLO THERE</h1>
-            <h2>Session: {session ? session.user.email : 'None'}</h2>
-            <button onClick={signIn} style={{ color: 'black', backgroundColor: 'yellow' }  }>Sign In</button>
-            <button onClick={signOut} style={{ color: 'black', backgroundColor: 'green' }} > Sign Out</button >               
+        <div className="App">           
+            <div>
+
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <h2>Session: {session ? session.user.email : 'None'}</h2>
+                    <p>Start of your schedule</p>
+                    <DateTimePicker label="start"></DateTimePicker>
+                    <p>End of your schedule</p>
+                    <DateTimePicker label="end"></DateTimePicker>
+                    <br></br>
+
+
+                    <button onClick={signIn} style={{ color: 'black', backgroundColor: 'yellow' }}>Sign In</button>
+                    <button onClick={signOut} style={{ color: 'black', backgroundColor: 'green' }} > Sign Out</button >       
+
+                    </LocalizationProvider>           
+            </div>
         </div>
+        
     );
 
-    //https://console.cloud.google.com/apis/credentials?hl=ko&project=calendarapi-420207
-
-    
 }
 
 export default App;
-
--- aspworks.client/src/main.jsx
+++ aspworks.client/src/main.jsx
@@ -5,11 +5,10 @@ import './index.css'
 import { createClient } from "@supabase/supabase-js"
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
-const supabase = createClient
-{
-    "https://qspejxvpnttjhiyphgjm.supabase.co"
-        , "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzcGVqeHZwbnR0amhpeXBoZ2ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI5OTM1NzAsImV4cCI6MjAyODU2OTU3MH0.SY_XrfBxnvbJ0YVItFRqZCnIduuYcZo9VRdHLJ4QRA8"
-}
+const supabase = createClient(
+    "https://qspejxvpnttjhiyphgjm.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzcGVqeHZwbnR0amhpeXBoZ2ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI5OTM1NzAsImV4cCI6MjAyODU2OTU3MH0.SY_XrfBxnvbJ0YVItFRqZCnIduuYcZo9VRdHLJ4QRA8"
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
